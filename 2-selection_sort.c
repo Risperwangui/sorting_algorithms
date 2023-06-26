d18d2cc7b11232673e2116ecfa607164fabfcd02
Risperwangui <resperwangui68@gmail.com>
2023-06-26 07:52:09 -0700

    writing a function that sorts an array of integers in ascending order

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -30,7 +30,7 @@ void selection_sort(int *array, size_t size)
 	{
 		minimum = array + i;
 		for (j = i + 1; j < size; j++)
-			minimum = (array[j] < *min) ? (array + j) : minimum;
+			minimum = (array[j] < *minimum) ? (array + j) : minimum;
 		if ((array + i) != minimum)
 		{
 			swap_ints(array + i, minimum);
